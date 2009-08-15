23fd2edd69d3570395118532cc6ba2c0d569e756
Tyler Church <tylertrain@yahoo.com>
2009-08-15 14:20:53 -0700

    Reversed the order in which pages appear

--- WIKI.tdb.rb
+++ WIKI.tdb.rb
@@ -1,8 +1,9 @@
-@mode = :div
-@inline_links = false
+@mode = :div # Set mode to div, instead of table, the default
+@inline_links = false # Don't give each attribute a link to its filter
 
-if cur == '/index' or cur == ''
-	@db.each do |pair|
+if cur == '/index' or cur == '' # If we are in the index listing
+	request.query["r"] = "1" # Reverse the order of the page listings
+	@db.each do |pair| # Make each title a link to its view page
 		@db[pair[0]]['Title'] = "<a href=\"/view/#{pair[0]}\">" + pair[1]['Title'] + "</a>"
 	end
 end

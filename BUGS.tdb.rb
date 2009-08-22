68e9e0e3a23a954e11410c2582615c2ddb5a7a38
Tyler Church <tylertrain@yahoo.com>
2009-08-22 12:28:41 -0700

    Added some comments to BUGS.tdb.rb

--- BUGS.tdb.rb
+++ BUGS.tdb.rb
@@ -1,9 +1,13 @@
 # Custom code for BUGS.tdb
 
+# Sets the default filter.
+# Is separated into a function, because it main be integrated
+# into a helper file.
 def default_filter filter, request
 	@db = filter(filter) unless !find_filters(request).empty?
 end
 
+# If we are in the index controller
 if cur == '' or cur == '/index'
 	default_filter({"Status" => ["Open"]}, request)
 end

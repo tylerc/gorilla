574ffa6ffb885250228c56c3f43af8241fc4af94
Tyler Church <tylertrain@yahoo.com>
2009-08-27 19:28:34 -0700

    Removed example code inside gorilla_mongrel.rb. Added command line arguments.

--- gorilla_mongrel.rb
+++ gorilla_mongrel.rb
@@ -84,78 +84,6 @@ class Simple < Mongrel::HttpHandler
 	end
 end
 
-h= Mongrel::HttpServer.new("0.0.0.0", "8000")
-h.register("/", Simple.new('BUGS/BUGS.tdb'))
-h.run.join
-
-=begin
-class CustomServlet < HTTPServlet::AbstractServlet
-	def initialize server, file
-		super server
-		@file = file
-		@db, @schema, @order = load_db(@file)
-		if !@schema.empty?
-			@schema.each_key do |key|
-				if @schema[key] == "ID"
-					@id_location = @order.index(key)
-					@order2 = @order.clone
-					@order2.delete_at @id_location
-				end
-			end
-		else
-			@id_location = 0
-			@order2 = []
-		end
-	end
-	
-	def do_GET(request, response)
-		response.status = 200
-		controllers = ['/index','/view', '/edit', '/delete', '/create', '/new', '/newprop', '/createprop', '/createprop2', '/editprop', '/deleteprop', '/reprop', '/reprop2']
-		index = '/index'
-		cur = ''
-		controllers.each do |controller|
-			if request.path.index(controller) != nil
-				cur = controller
-			end
-		end
-		eval File.read("default_conf.rb"), binding
-		Dir.glob("helpers/*.rb").each do |helper|
-			load helper
-		end
-		if File.exists?(@file + '.rb')
-			eval File.read(@file + '.rb')
-		end
-		if controllers.index(cur) != nil or request.path == '/'
-			response['Content-Type'] = "text/html"
-			erb_text = ""
-			File.open('views/header.html.erb', 'r') do |file|
-				erb_text += file.read
-			end
-			file_name = ""
-			if request.path == '/'
-				file_name = "views/#{index}.html.erb"
-			else
-				p request.path
-				file_name = "views#{cur}.html.erb"
-			end
-			File.open(file_name, 'r') do |file|
-				erb_text += file.read
-				file.close
-			end
-			File.open('views/footer.html.erb', 'r') do |file|
-				erb_text += file.read
-			end
-			response.body += ERB.new(erb_text).result(binding)
-		end
-		if request.path.split('.')[-1] == 'css'
-			response['Content-Type'] = 'text/css'
-			response.body = File.new(request.path[1..-1],'r').readlines.join
-		end
-	end
-	
-	alias :do_POST :do_GET
-end
-
 if __FILE__ == $0
 	file = 'BUGS/BUGS.tdb'
 	port = 8000
@@ -168,15 +96,14 @@ if __FILE__ == $0
 		end
 	end
 	# Mount servlets.
-	s = HTTPServer.new(:Port => port, :MimeTypes =>  WEBrick::HTTPUtils::DefaultMimeTypes, :DocumentRoot => Dir.pwd)
-	s.mount('/', CustomServlet, file)
+	h= Mongrel::HttpServer.new("0.0.0.0", "8000")
+	h.register("/", Simple.new(file))
 
 	# Trap signals so as to shutdown cleanly.
 	['TERM', 'INT'].each do |signal|
-		trap(signal) {s.shutdown}
+		trap(signal) {h.stop}
 	end
 
 	# Start the server and block on input
-	s.start
+	h.run.join
 end
-=end

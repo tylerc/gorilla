6869a92a842c6db6cba639c870e485507f3bf3c5
Tyler Church <tylertrain@yahoo.com>
2009-08-28 22:30:19 -0700

    We now measure time it takes to save

--- test/stress.rb
+++ test/stress.rb
@@ -1,5 +1,6 @@
 Dir.chdir File.expand_path(File.dirname(__FILE__))
 
+require 'benchmark'
 require '../db'
 
 # Creates a database for stress testing
@@ -10,12 +11,12 @@ db = {}
 schema = {"Status"=>["Open", "Fixed"], "Milestone"=>"STRING", "Priority"=>["Low", "Medium", "High"], "Description"=>"LSTRING", "Bug"=>"ID", "Type"=>["Defect", "Blocker", "Enhancement", "Feature Request"]}
 order = ["Bug", "Description", "Type", "Status", "Priority", "Milestone"]
 data = {"Description" => ["test"], "Type" => ['Defect', 'Blocker', 'Enhancement', 'Feature Request'], "Status" => ['Open', 'Fixed'], "Priority" => ['Low', 'Medium', 'High'], "Milestone" => ["1.1"]}
-10.times do |num|
+1_000.times do |num|
 	order[1..-1].each do |item|
 		db[num+1] = {} if db[num+1].nil?
 		db[num+1][item] = data[item][rand(data[item].length)]
 	end
 end
 puts "Done creating"
-save_db db, schema, order, "STRESS.tdb"
+puts Benchmark.measure { save_db db, schema, order, "STRESS.tdb" }
 puts "Done Writing..."

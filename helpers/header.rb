62b811db13453b26778e1d58389a9340c23a00f6
Tyler Church <tylertrain@yahoo.com>
2009-10-01 21:22:58 -0700

    Removed usage of @db that should've been db

--- helpers/header.rb
+++ helpers/header.rb
@@ -63,8 +63,8 @@ def list(db, request)
 		text << "<td>#{link}</td>" if request.query["mode"] == "table"
 		text << "<div class='wrapper'><div class='#{@order[@id_location]}'>#{link}</div>" if request.query["mode"] == "div"
 		@order2.each do |item|
-			link = "<a class='intable' href='/?filter#{item}#{@db[key][item]}'>#{ @db[key][item] }</a>" if request.query["inline_links"] == "true"
-			link = "#{ @db[key][item] }" if request.query["inline_links"] == "false"
+			link = "<a class='intable' href='/?filter#{item}#{db[key][item]}'>#{ db[key][item] }</a>" if request.query["inline_links"] == "true"
+			link = "#{ db[key][item] }" if request.query["inline_links"] == "false"
 			text << "<td>#{link}</td>" if request.query["mode"] == "table"
 			text << "<div class='#{item}'>#{link}</div>" if request.query["mode"] == "div"
 		end
